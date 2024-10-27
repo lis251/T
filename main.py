604548d99795ebebe490061710f196e00dbb4b52
unknown <oleff@mail.ru>
2024-10-28 01:41:26 +0300

    11

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import pandas as pd
 import requests
 from matplotlib import pyplot as plt
+from datetime import date
 
-j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/SBER/candles.json?from=2024-01-01&till=2024-10-26&interval=24').json()
+# Получаем текущую дату
+today = date.today()
+
+# Преобразуем её в строку в нужном формате
+formatted_date = today.strftime('%Y-%m-%d')
+
+j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/SBER/candles.json?from=2024-01-01&till='+formatted_date+'&interval=24').json()
 data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
 
 pd.DataFrame(data).to_csv('files/sber.csv')
+
+
+j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/YDEX/candles.json?from=2024-01-01&till='+formatted_date+'&interval=24').json()
+data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
+
+pd.DataFrame(data).to_csv('files/ydex.csv')
+
+
+j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/TCSG/candles.json?from=2024-01-01&till='+formatted_date+'&interval=24').json()
+data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
+
+pd.DataFrame(data).to_csv('files/tcsg.csv')
+
 #print(j)
 #print(pd.DataFrame(data))
 
