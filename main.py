38757b6eeadd16d4b02ee47d3ed51a138fb49f8d
unknown <oleff@mail.ru>
2024-10-27 15:03:27 +0300

    date start year

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 from matplotlib import pyplot as plt
 
-j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/SBER/candles.json?from=2024-10-01&till=2024-10-26&interval=24').json()
+j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/SBER/candles.json?from=2024-01-01&till=2024-10-26&interval=24').json()
 data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
 
 pd.DataFrame(data).to_csv('sber.csv')
