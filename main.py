763ba72647a18bbfa85d43601569eaf1c7071a14
unknown <oleff@mail.ru>
2024-10-27 15:18:11 +0300

    add folder filess

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/SBER/candles.json?from=2024-01-01&till=2024-10-26&interval=24').json()
 data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
 
-pd.DataFrame(data).to_csv('sber.csv')
+pd.DataFrame(data).to_csv('files/sber.csv')
 #print(j)
 print(pd.DataFrame(data))
 
