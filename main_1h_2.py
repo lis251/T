541f974f327febf8c2cb78ba7c36acd896976860
unknown <oleff@mail.ru>
2024-11-19 01:28:16 +0300

    123

--- main_1h_2.py
+++ main_1h_2.py
@@ -2,7 +2,8 @@ import pandas as pd
 import requests
 import datetime
 from matplotlib import pyplot as plt
-from datetime import date
+from datetime import datetime, timedelta
+import sys
 
 
 class Item:
@@ -13,14 +14,14 @@ class Item:
     def __repr__(self):
         return f"Item(name='{self.name}', value={self.value})"
 
-def main(instr):
+def main(instr,dateStart,dateEnd):
 
-    today = date.today()
+    #today = date.today()
 
     # Преобразуем её в строку в нужном формате
-    formatted_date = today.strftime('%Y-%m-%d')
+    #formatted_date = today.strftime('%Y-%m-%d')
 
-    j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/'+instr+'/candles.json?from=2024-10-01&till='+formatted_date+'&interval=10').json()
+    j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/'+instr+'/candles.json?from='+dateStart+'&interval=10').json()
     data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
 
     #for index, value in enumerate(my_list):
@@ -30,8 +31,8 @@ def main(instr):
     # Перебираем массив и добавляем новый объект в каждый элемент
     new_items = []
     for item in data:
-        dt_begin = datetime.datetime.strptime(item['begin'], "%Y-%m-%d %H:%M:%S")
-        dt_end = datetime.datetime.strptime(item['end'], "%Y-%m-%d %H:%M:%S")
+        dt_begin = datetime.strptime(item['begin'], "%Y-%m-%d %H:%M:%S")
+        dt_end = datetime.strptime(item['end'], "%Y-%m-%d %H:%M:%S")
         # Извлекаем нужные значения
         year = dt_begin.year
         month = dt_begin.month
@@ -47,19 +48,53 @@ def main(instr):
         item['hour']=hour;
         item['min_begin']=minute_begin;
         item['min_end']=minute_end;
-        new_items.append(item)
+        if dateEnd<dt_begin:
+            new_items.append(item)
         #new_item = Item(f"new_{item.name}", item.value + 500)
         #new_items.append(new_item)
+
+    return new_items;
         
         
     # Объединяем исходный массив с новым
     #result = data + new_items
     
     # Выводим результат
-    pd.DataFrame(data).to_csv('files/'+instr+'_10min.csv')
-    print('Save - '+instr+'_10min.csv')
+    #pd.DataFrame(data).to_csv('files/'+instr+'_10min.csv')
+    #print('Save - '+instr+'_10min.csv')
 
-if __name__ == "__main__":    
+if __name__ == "__main__":   
+
+    arguments = sys.argv[1:]
+    days = arguments[0]
+
+    instrAll= ['YDEX','SBER','TCSG','LKOH','AGRO','TATN','TRNFP','CHMF','MAGN','UPRO']
+
+
+    for instr in instrAll:
+
+        # Устанавливаем начальную дату
+        #start_date = datetime(2024, 1, 1)
+        
+        stepSart = timedelta(days=int(days))
+        start_date = datetime.now()-stepSart
+        current_date = datetime.now()
+        step = timedelta(days=5)
+
+        date = start_date
+        dataAll = []
+        while date <= current_date:
+            formatted_date = date.strftime("%Y-%m-%d")
+            #print(formatted_date)
+            dataAll=dataAll+main(instr,formatted_date,date+step);    
+            date += step
+        
+        pd.DataFrame(dataAll).to_csv('files/'+instr+'_10min.csv')
+        print('Save - '+instr+'_10min.csv')
+    
+
+
+    """
     main('YDEX')
     main('SBER')
     main('TCSG')
@@ -70,76 +105,5 @@ if __name__ == "__main__":
     main('CHMF')
     main('MAGN')
     main('UPRO')
+    """
 
-# Получаем текущую дату
-
-#print(data[1])
-#data[1].insert('year', 2024)
-#print(data[1])
-
-#pd.DataFrame(data).to_csv('files/sber_1h.csv')
-
-"""
-j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/YDEX/candles.json?from=2024-01-01&till='+formatted_date+'&interval=24').json()
-data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
-
-pd.DataFrame(data).to_csv('files/ydex_1h.csv')
-
-
-j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/TCSG/candles.json?from=2024-01-01&till='+formatted_date+'&interval=24').json()
-data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
-
-pd.DataFrame(data).to_csv('files/tcsg_1h.csv')
-
-
-j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/LKOH/candles.json?from=2024-01-01&till='+formatted_date+'&interval=24').json()
-data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
-
-pd.DataFrame(data).to_csv('files/lkoh_1h.csv')
-
-
-j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/AGRO/candles.json?from=2024-01-01&till='+formatted_date+'&interval=24').json()
-data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
-
-pd.DataFrame(data).to_csv('files/agro_1h.csv')
-
-
-j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/TATN/candles.json?from=2024-01-01&till='+formatted_date+'&interval=24').json()
-data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
-
-pd.DataFrame(data).to_csv('files/tatn_1h.csv')
-
-
-j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/TRNFP/candles.json?from=2024-01-01&till='+formatted_date+'&interval=24').json()
-data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
-
-pd.DataFrame(data).to_csv('files/trnfp_1h.csv')
-
-
-j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/CHMF/candles.json?from=2024-01-01&till='+formatted_date+'&interval=24').json()
-data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
-
-pd.DataFrame(data).to_csv('files/chmf_1h.csv')
-
-
-j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/MAGN/candles.json?from=2024-01-01&till='+formatted_date+'&interval=24').json()
-data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
-
-pd.DataFrame(data).to_csv('files/magn_1h.csv')
-
-
-j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/UPRO/candles.json?from=2024-01-01&till='+formatted_date+'&interval=24').json()
-data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
-
-pd.DataFrame(data).to_csv('files/upro_1h.csv')
-
-"""
-
-#print(j)
-#print(pd.DataFrame(data))
-
-
-#frame = pd.DataFrame(data)
-#plt.plot(list(frame['clos
-# e']))
-#plt.savefig("shares.png")
